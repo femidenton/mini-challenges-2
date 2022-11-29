079e0a181fc34623730d502826d18ee4107f684e
Femi Denton <femidenton@yahoo.co.uk>
2022-11-29 20:53:18 +0100

    Mini challenges 2 completed

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,30 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    let isolatedStr = "";
+    let mapper = {};
+    let counter = 0;
 
+    if (typeof text !== "string") {
+        throw new Error("Please enter a valid strings")
+    } else {
+        const str = text.toUpperCase();
+
+        for (let i = 0; i < text.length; i++) {
+            if (str[i].repeat(2) === str[i - 2] + str[i - 1]) {
+
+                if (!mapper[str[i]]) {
+                    mapper[str[i]] = text[i];
+                } else {
+                    mapper[str[i]] += text[i]
+                }
+
+                if (str[i] !== str[i + 1]) {
+                    isolatedStr += "[" + mapper[str[i]] + "]";
+                    counter += 1;
+                    mapper = {};
+                }
+            } else { isolatedStr += text[i]; }
+        }
+        return [isolatedStr, counter]
+    }
+}
 module.exports = isolateDuplicates;
